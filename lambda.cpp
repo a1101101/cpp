5fe3a5d4781a020dd13b94f73b705857bc533156
sweetbaby0 <r3901@icloud.com>
2019-10-09 02:36:37 +0900

    Add files via upload

---
+++ lambda.cpp
@@ -0,0 +1,22 @@
+/*
+- int mainの中で関数処理がしたい
+*/
+#include <iostream>
+using namespace std;
+
+void func(int i, int j){
+	cout << i+j << endl;
+}
+
+int main(){
+	int a; int b;
+	cin >> a >> b;
+	
+	func(a, b);
+	
+	//lambda expression
+	auto lfunc = [](int i, int j){cout << i+j;};
+	lfunc(a, b);
+	
+	return 0;
+}
