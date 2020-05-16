cecf541cf0848befeb6810b4b26a66b4efc04c51
a1101101 <r3901@icloud.com>
2020-05-16 11:42:49 +0900

    Update lambda2.cpp

--- basics/lambda2.cpp
+++ basics/lambda2.cpp
@@ -1,22 +1,24 @@
-/*
-ラムダ式のキャプチャ演算子を用いて、スコープ外の変数を操作する例
-*/
+//ラムダ式のキャプチャ演算子を用いて、スコープ外の変数を操作する例
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
+	//変数と入力
 	int n; cin>>n;
 	vector<int> v;
 	
+	//fの定義
 	auto f = [&v](int n){
 		for(int i=0;i<n;i++){
 			v.push_back(i);
 		}
 	};
 	
+	//fの実行
 	f(n);
-	cout<<v.size()<<endl;
+	
+	//vの中身
 	for(int i=0;i<v.size();i++){
 		cout<<v[i]<<' ';
 	}
