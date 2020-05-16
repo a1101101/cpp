14c440f3c1722ced29c4d0c5f27ba88775240c3c
a1101101 <r3901@icloud.com>
2020-05-16 15:50:50 +0900

    Add files via upload

---
+++ algorithm/rec2.cpp
@@ -0,0 +1,30 @@
+//ループの再帰定義
+#include <iostream>
+using namespace std;
+
+//単位処理
+void prt(int i){
+	cout<<i<<endl;
+}
+
+//再帰定義
+void rec(int n){
+	if(n==0){
+		return;}
+	else{
+		rec(n-1);
+		prt(n-1);
+	}
+}
+
+int main(){
+	int n; scanf("%d",&n);
+	
+	//ループ
+	for(int i=0;i<n;i++){prt(i);}
+	
+	//再帰
+	rec(n);
+	
+	return 0;
+}
