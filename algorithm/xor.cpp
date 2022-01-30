bec853a38c0724b6abad7e0da7e1f211621ca52e
a1101101 <r3901@icloud.com>
2022-01-30 13:51:06 +0900

    Add files via upload

---
+++ algorithm/xor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "Enter integer a: ";
+    cin >> a;
+    cout << "\n a: " << 
+    cout << "Enter integer b: ";
+    cin >> b;
+
+    cout << "a ^ b : " << (a ^ b);
+
+    return 0;
+}
